2599a2f93135b7c2eed071c65e6bba1d1a31f6a7
Nora Dunleavy <noradunleavyy@gmail.com>
2018-07-20 14:02:40 -0500

    Python handlers update and links to html

--- main.py
+++ main.py
@@ -11,17 +11,27 @@ jinja_env = jinja2.Environment(
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        response_html = jinja_env.get_template('templates/index.html')
+        self.response.write(response_html.render())
 
 class LoginPageHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        response_html = jinja_env.get_template('templates/login.html')
+        self.response.write(response_html.render())
 
 class ViewItemHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        response_html = jinja_env.get_template('templates/search.html')
+        self.response.write(response_html.render())
 
 class ChecklistHandler(webapp2.RequestHandler):
     def get(self):
-
-
+        self.response.headers['Content-Type'] = 'text/html'
+        response_html = jinja_env.get_template('templates/checklist.html')
+        self.response.write(response_html.render())
 
 app = webapp2.WSGIApplication([
     ('/', WelcomeHandler),
