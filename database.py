1e4f3e2d1c24a6f2c272823ec68cd7d0479c3fdc
Abenazer Mekete <abenazer101@gmail.com>
2018-07-23 08:25:16 -0700

    added the extra iframes

--- database.py
+++ database.py
@@ -7,10 +7,15 @@ class DatabaseWants(ndb.Model):
 	value = ndb.StringProperty()
 
 class DatabaseBought(ndb.Model):
+<<<<<<< HEAD
 
 	value = ndb.StringProperty()
 
 
 
+=======
+	value = ndb.StringProperty()
+
+>>>>>>> 5b87ea617eecf15077bb2fcdbeb9758f9a6b7705
 # use value and catagory after giving add buttons different key values
 # >>>>>>> a49925f9b6e3d54b05b46ba375e8b5d336c2acf9
