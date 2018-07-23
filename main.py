0ab0d36438084aac3def740880e8e6e985022294
Blossom Mojekwu <blossommojekwu@gmail.ccom>
2018-07-23 15:34:55 -0500

    checklist js

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ class ChecklistHandler(webapp2.RequestHandler):
         typeSelector = self.request.get('choice')
         self.response.headers['Content-Type'] = 'text/html'
         stored_items = database.DatabaseEntry(type= typeSelector, value= item)
-        self.response.write(item)
         stored_items.put()
+
 
 
 app = webapp2.WSGIApplication([
